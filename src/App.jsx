c6f4079d45370a0c5e1a58c52f56328dbb792f16
Bizzy-coding <bizzycoding@gmail.com>
2023-04-09 12:49:13 +0100

    initial folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from 'react'
-
+import Contact from './Components/Contact'
+import Header from './Components/Header'
+import Main from './Components/Main'
+import Navbar from './Components/Navbar'
 import './App.css'
 
+
 function App() {
-  return <h1>hello</h1>
+  return ( 
+    <div className='main-container'>
+      <Navbar />
+      <Header />
+      <Main />
+      <Contact />
+    </div>
+  
+  )
+ 
 }
 
 export default App
